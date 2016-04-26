d5e637429b9074409278bd80fcba9e6fed2000cf
Kuba Żerko <zerasop@gmail.com>
2016-04-26 19:21:18 +0200

    deleted unnecessary methods

--- src/main/java/com/appunite/debughelper/DebugHelperPreferences.java
+++ src/main/java/com/appunite/debughelper/DebugHelperPreferences.java
@@ -8,7 +8,6 @@ import javax.annotation.Nonnull;
 public class DebugHelperPreferences {
 
     private static final String DEBUG_DRAWER_PREFS = "debug_drawer_prefs";
-    private static final String LEAK_CANARY_STATE = "canary_state";
     private static final String DEBUG_MODE = "debug_helper_mode_status";
 
     @Nonnull
@@ -18,20 +17,11 @@ public class DebugHelperPreferences {
         sharedPreferences = context.getSharedPreferences(DEBUG_DRAWER_PREFS, 0);
     }
 
-    public void saveLeakCanaryState(boolean state) {
-        final SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putBoolean(LEAK_CANARY_STATE, state);
-        editor.apply();
-    }
 
     public void saveDebugState(boolean state) {
         final SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putBoolean(DEBUG_MODE, state);
         editor.apply();
-    }
-
-    public boolean getLeakCanaryState() {
-        return sharedPreferences.getBoolean(LEAK_CANARY_STATE, false);
     }
 
     public boolean getDebugState() {
