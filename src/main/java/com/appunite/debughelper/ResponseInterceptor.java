51f75b10b7c98e013edfbcc4920b11c993cca953
Kuba Żerko <zerasop@gmail.com>
2016-04-28 15:32:34 +0200

    now interceptor response can be used in other interceptors

--- src/main/java/com/appunite/debughelper/ResponseInterceptor.java
+++ src/main/java/com/appunite/debughelper/ResponseInterceptor.java
@@ -8,8 +8,6 @@ import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.List;
 
-import javax.annotation.Nonnull;
-
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -24,7 +22,29 @@ public class ResponseInterceptor implements Interceptor {
 
     @Override
     public Response intercept(Chain chain) throws IOException {
-        Response newResponse;
+        Response response = fakeResponse(chain);
+        return response;
+    }
+
+    public static void setResponseCode(int responseCode) {
+        ResponseInterceptor.responseCode = responseCode;
+    }
+
+    public static void setEmptyResponse(boolean emptyResponse) {
+        ResponseInterceptor.emptyResponse = emptyResponse;
+    }
+
+    public static int getResponseCode() {
+        return responseCode;
+    }
+
+    public static boolean getEmptyResponse() {
+        return emptyResponse;
+    }
+
+
+    public static Response fakeResponse(Chain chain) throws IOException {
+        Response.Builder newResponse;
         Gson gson = new Gson();
         List<Object> arrayList = ImmutableList.of(new Object());
         String emptyJson;
@@ -51,48 +71,13 @@ public class ResponseInterceptor implements Interceptor {
         } else {
             emptyJson = gson.toJson(arrayList);
         }
-
+        newResponse = response.newBuilder();
         if (emptyResponse) {
-            newResponse = response.newBuilder()
-                    .body(ResponseBody.create(response.body().contentType(), emptyJson))
-                    .code(responseCode).build();
+            newResponse.body(ResponseBody.create(response.body().contentType(), emptyJson));
         } else {
-            newResponse = response.newBuilder()
-                    .body(ResponseBody.create(response.body().contentType(), bodyString))
-                    .code(responseCode)
-                    .build();
+            newResponse.body(ResponseBody.create(response.body().contentType(), bodyString));
         }
-        return handleError(newResponse);
-    }
-
-    @Nonnull
-    public Response handleError(Response response) throws IOException {
-        if (response.isSuccessful()) {
-            return response;
-        }
-        final int statusCode = response.code();
-        if (statusCode < 200) {
-            return response;
-        }
-
-        throw new IOException("Exception code " + response.code());
-    }
-
-
-    public static void setResponseCode(int responseCode) {
-        ResponseInterceptor.responseCode = responseCode;
-    }
-
-    public static void setEmptyResponse(boolean emptyResponse) {
-        ResponseInterceptor.emptyResponse = emptyResponse;
-    }
-
-    public static int getResponseCode() {
-        return responseCode;
-    }
-
-    public static boolean getEmptyResponse() {
-        return emptyResponse;
+        return newResponse.code(responseCode).message("MOCK").build();
     }
 
 }
